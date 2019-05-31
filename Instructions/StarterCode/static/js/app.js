79ee01f468b6012923485c496a0af2cb24ca8a03
LuiCel <45442962+LSceleste@users.noreply.github.com>
2019-05-31 09:43:00 -0700

    Part 1 complete

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+function buildTable(data1) {
+    tbody.html(""); 
+    data1.forEach((dataRow) => {
+        var row = tbody.append("tr");
+        Object.entries(dataRow).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+
+}
+
+function processClick() {   
+    d3.event.preventDefault();
+    var inputDate = d3.select("#datetime").property("value");
+    console.log(inputDate)
+    var filteredData = tableData 
+    if (inputDate) {
+        filteredData = filteredData.filter(row => row.datetime == inputDate);
+        console.log(filteredData)
+    }
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", processClick);
+
+
+buildTable(tableData)
